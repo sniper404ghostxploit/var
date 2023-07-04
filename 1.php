4318206eeac5bf0dae30eddad36a0594d6691659
sniper404ghostxploit <115604134+sniper404ghostxploit@users.noreply.github.com>
2023-07-04 22:37:00 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,5 +1,5 @@
-<?php
+
 $CANONICAL='https://football-corner.org/wp-content/uploads/2023/-/';
 $REFFERAL='https://heylink.me/pasticuan5000/';
 $GSC='xehCUO0-etJQziL4D03Zf0-s_nGPB3UMe4HyxXZ37Qc';
-?>
+
